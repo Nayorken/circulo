caea2e88bffe1b6be47e541182ca22a598caf9d2
nayorken <hugo.ferreira1617@gmail.com>
2019-11-26 19:01:25 +0000

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace circulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        circu c = new circu();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.pi = 3.14159265359;
+            c.raio = Convert.ToDouble(textBox1.Text);
+            if (radioButton1.Checked) label2.Text = c.area;
+            else if (radioButton2.Checked) label2.Text = c.perimetro;
+            else if (radioButton3.Checked) label2.Text = c.diagonal;
+        }
+    }
+}
---
+++ circu.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace circulo
+{
+    class circu
+    {
+        public double pi;
+        public double raio;
+        public string area => ((pi) * (raio)).ToString();
+        public string perimetro => ((pi)+(pi) * (raio)).ToString();
+        public string diagonal => ((raio)*2).ToString();
+    }
+}
